c9687b08d5efc852c60697114093925f46b1b896
Erick Santiago Melgar Jara <developer.pideya@gmail.com>
2021-01-05 23:50:28 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user-dev:4WwcBH0J76MozY9U@cluster0.pxde3.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
